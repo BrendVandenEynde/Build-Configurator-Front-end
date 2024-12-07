083833b7428078fb698e338f28f56f760c9ec2a7
Clueless <brendvandeneynde@hotmail.be>
2024-12-07 23:03:10 +0100

    feat: add Layout component with header and slot
    - Created the Layout.vue component and added it to the components directory.
    
    - This component includes a header with a logo and a login button.
    
    - Also uses a slot to render child content, allowing for flexible layout integration within the app.

---
+++ src/components/Layout.vue
@@ -0,0 +1,47 @@
+<script setup>
+</script>
+
+<template>
+    <header class="headerMainfront">
+        <div class="headerMain_logo">
+            <img src="https://sneaker-configurator.vercel.app/media/SWEAR_Logo.webp" alt="Swear logo"
+                class="headerLogo">
+        </div>
+        <button class="headerMain_login" @click="$emit('navigateToLogin')">Login</button>
+    </header>
+
+    <main>
+        <slot></slot> <!-- This slot will render the content of App.vue -->
+    </main>
+
+    <footer>
+        <!-- You can add a footer if needed -->
+    </footer>
+</template>
+
+<style scoped>
+.headerMainfront {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+}
+
+.headerMain_logo img {
+    height: 40px;
+}
+
+.headerMain_login {
+    width: 100px;
+    padding: 10px 15px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.headerMain_login:hover {
+    background-color: #0056b3;
+}
+</style>
